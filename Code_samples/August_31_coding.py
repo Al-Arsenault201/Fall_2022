a9a5bbe31303e0755e9c4eba12aa5821a48bfcb8
Al-Arsenault201 <69997839+Al-Arsenault201@users.noreply.github.com>
2022-09-06 20:00:45 -0400

    Create August_31_coding.py

---
+++ Code_samples/August_31_coding.py
@@ -0,0 +1,22 @@
+# This is a comment
+# it will not be executed
+# it is only for human consumption; the computer doesn't care
+
+print("Hello, world") # the comment is the rest of the line
+
+"""
+This is a block comment
+Used for identification and for detailed explanations of complex code
+
+Name: Alfred W. Arsenault
+Class: CMSC 201
+Semester: Fall 2022
+Date: August 31 
+Assignment: First coding
+
+
+
+"""
+print ("Hello, world")
+print ("Hello, world is boring. Let's do something else")
+print ('hey, single quotes work too")
